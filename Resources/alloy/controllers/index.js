4df657ec01e549157918bccf187d7572aac9fa93
Angel Astudillo <xavieras1@gmail.com>
2013-11-01 10:21:14 -0500

    move the pin

--- Resources/alloy/controllers/index.js
+++ Resources/alloy/controllers/index.js
@@ -1,7 +1,4 @@
 function Controller() {
-    function doClick() {
-        alert($.label.text);
-    }
     require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
     this.__controllerPath = "index";
     arguments[0] ? arguments[0]["__parentSymbol"] : null;
@@ -9,7 +6,6 @@ function Controller() {
     arguments[0] ? arguments[0]["__itemTemplate"] : null;
     var $ = this;
     var exports = {};
-    var __defers = {};
     $.__views.index = Ti.UI.createWindow({
         backgroundColor: "white",
         id: "index"
@@ -17,8 +13,8 @@ function Controller() {
     $.__views.index && $.addTopLevelView($.__views.index);
     var __alloyId1 = [];
     $.__views.mountainView = Ti.Map.createAnnotation({
-        latitude: 37.390749,
-        longitude: -122.081651,
+        latitude: 0,
+        longitude: -82.081651,
         id: "mountainView",
         title: "Appcelerator Headquarters",
         subtitle: "Mountain View, CA",
@@ -37,13 +33,9 @@ function Controller() {
         mapType: Ti.Map.STANDARD_TYPE
     });
     $.__views.index.add($.__views.mapview);
-    doClick ? $.__views.mapview.addEventListener("click", doClick) : __defers["$.__views.mapview!click!doClick"] = true;
-    setRegion ? $.__views.mapview.addEventListener("complete", setRegion) : __defers["$.__views.mapview!complete!setRegion"] = true;
     exports.destroy = function() {};
     _.extend($, $.__views);
     $.index.open();
-    __defers["$.__views.mapview!click!doClick"] && $.__views.mapview.addEventListener("click", doClick);
-    __defers["$.__views.mapview!complete!setRegion"] && $.__views.mapview.addEventListener("complete", setRegion);
     _.extend($, exports);
 }
 
