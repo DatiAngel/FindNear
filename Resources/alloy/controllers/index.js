006dd89d1dcc5180b8fc7486d6cadcd5479c7b5c
Angel Astudillo <xavieras1@gmail.com>
2013-11-01 10:13:02 -0500

    map

--- Resources/alloy/controllers/index.js
+++ Resources/alloy/controllers/index.js
@@ -15,19 +15,35 @@ function Controller() {
         id: "index"
     });
     $.__views.index && $.addTopLevelView($.__views.index);
-    $.__views.label = Ti.UI.createLabel({
-        width: Ti.UI.SIZE,
-        height: Ti.UI.SIZE,
-        color: "#000",
-        text: "Hello, World",
-        id: "label"
+    var __alloyId1 = [];
+    $.__views.mountainView = Ti.Map.createAnnotation({
+        latitude: 37.390749,
+        longitude: -122.081651,
+        id: "mountainView",
+        title: "Appcelerator Headquarters",
+        subtitle: "Mountain View, CA",
+        pincolor: Titanium.Map.ANNOTATION_RED,
+        leftButton: "/images/appcelerator_small.png",
+        myid: "1"
     });
-    $.__views.index.add($.__views.label);
-    doClick ? $.__views.label.addEventListener("click", doClick) : __defers["$.__views.label!click!doClick"] = true;
+    __alloyId1.push($.__views.mountainView);
+    $.__views.mapview = Ti.Map.createView({
+        annotations: __alloyId1,
+        id: "mapview",
+        ns: Ti.Map,
+        animate: "true",
+        regionFit: "true",
+        userLocation: "true",
+        mapType: Ti.Map.STANDARD_TYPE
+    });
+    $.__views.index.add($.__views.mapview);
+    doClick ? $.__views.mapview.addEventListener("click", doClick) : __defers["$.__views.mapview!click!doClick"] = true;
+    setRegion ? $.__views.mapview.addEventListener("complete", setRegion) : __defers["$.__views.mapview!complete!setRegion"] = true;
     exports.destroy = function() {};
     _.extend($, $.__views);
     $.index.open();
-    __defers["$.__views.label!click!doClick"] && $.__views.label.addEventListener("click", doClick);
+    __defers["$.__views.mapview!click!doClick"] && $.__views.mapview.addEventListener("click", doClick);
+    __defers["$.__views.mapview!complete!setRegion"] && $.__views.mapview.addEventListener("complete", setRegion);
     _.extend($, exports);
 }
 
